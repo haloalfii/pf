468bf8fd2dcb08f6c6879108f29b68f27f071e49
Alfian Luthfi <alfiankurniawan85@gmail.com>
2023-03-21 06:10:43 +0700

    Adding BS Done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,38 @@
 <template>
-    <div>
-        <h1>Initial</h1>
+    <div class="col-xxl-12 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img
+                    src="../assets/images/alfi.png"
+                    class="d-block mx-lg-auto img-fluid"
+                    alt="Alfian Luthfi"
+                    width="630"
+                    height="450"
+                    loading="lazy"
+                />
+            </div>
+            <div class="col-lg-6">
+                <!-- <p class="lead text-muted">Hello There!</p> -->
+                <h1 class="display-5 fw-bold lh-1 mb-3">
+                    Hello There!.<br />
+                    I'm <span class="text-primary">Alfian Luthfi</span>
+                </h1>
+                <p class="lead">Indonesian-based</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button
+                        type="button"
+                        class="btn btn-primary btn-lg px-4 me-md-2"
+                    >
+                        Primary
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-outline-secondary btn-lg px-4"
+                    >
+                        Default
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
