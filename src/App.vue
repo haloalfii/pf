2a29fe45211721f452e4a792ce91abc9ec198e84
Alfian Luthfi <alfiankurniawan85@gmail.com>
2023-03-21 20:42:28 +0700

    Edu and Footer Done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-    <div id="app" class="container py-3">
-        <Navbar />
-        <RouterView />
+    <div id="app" class="position-relative">
+        <div
+            @click="themeToggle"
+            title="Change to Dark Mode"
+            class="mode-toggle d-flex align-items-center justify-content-center cursor-pointer"
+        >
+            <p class="mb-0" style="font-size: 28px">☀️</p>
+        </div>
+        <div class="container py-3">
+            <Navbar />
+            <RouterView />
+        </div>
     </div>
 </template>
 
@@ -12,6 +21,17 @@ import Navbar from "./components/Navbar.vue";
 export default {
     components: {
         Navbar,
+    },
+
+    methods: {
+        themeToggle() {
+            var element = document.getElementsByTagName("html")[0];
+            if (element.getAttribute("data-bs-theme") == "dark") {
+                element.setAttribute("data-bs-theme", "light");
+            } else {
+                element.setAttribute("data-bs-theme", "dark");
+            }
+        },
     },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer
+        class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top"
+    >
+        <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
+
+        <a
+            href="/"
+            class="col-md-4 d-flex align-items-center justify-content-center d-none d-lg-flex mb-md-0 me-md-auto link-dark text-decoration-none"
+        >
+            <i
+                class="fa-solid fa-heart text-danger"
+                style="font-size: 24px"
+            ></i>
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item">
+                <a href="#" class="nav-link ps-0 pe-2 text-muted">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link ps-0 pe-2 text-muted">About</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link ps-0 pe-2 text-muted">Pricing</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link ps-0 pe-2 text-muted">FAQs</a>
+            </li>
+        </ul>
+    </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
